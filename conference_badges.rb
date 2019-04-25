6d4266a994b3aa788e978ecc2858b9a3e227203e
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-25 23:57:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,16 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(ar)
-  ar.each()
+  ar.each( |name| badge_maker(name))
+end
+
+def assign_rooms(ar)
+  rooms = (1..ar.count)
+  for room in rooms do
+    "Hello, #{ar[room]}! You'll be assigned to room #{room}!"
+  end
+end
+
+def printer(speakers)
+  puts batch_badge_creator(speakers)
 end
